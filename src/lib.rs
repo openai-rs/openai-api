0f48bb1fb7806caff91dc24d0383a5f2e2945de9
Guantong <i@guantong.dev>
2023-03-27 16:46:58 +0800

    Impl Display for Error

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,8 @@
 #![warn(unused_crate_dependencies)]
 
 pub mod apis;
+use std::fmt::{Display, Formatter, self};
+
 pub use apis::*;
 pub mod openai;
 pub use openai::*;
@@ -19,4 +21,13 @@ pub enum Error {
 	ApiError(String),
 	/// An Error not related to the API
 	RequestError(String),
+}
+
+impl Display for Error {
+    fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
+        match self {
+            Error::ApiError(msg) => write!(f, "API error: {}", msg),
+            Error::RequestError(msg) => write!(f, "Request error: {}", msg),
+        }
+    }
 }
