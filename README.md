3391492f00d6cf2ff1b49ea63a2180c801fa3e30
suddenlyAstral <yonatanw55@gmail.com>
2025-02-22 17:15:55 +0200

    renamed max_tokens -> max_completion_tokens in ChatBody to support reasoning models. added reasoning model test

--- README.md
+++ README.md
@@ -53,7 +53,7 @@ fn main() {
     let openai = OpenAI::new(auth, "https://api.openai.com/v1/");
     let body = ChatBody {
         model: "gpt-3.5-turbo".to_string(),
-        max_tokens: Some(7),
+        max_completion_tokens: Some(7),
         temperature: Some(0_f32),
         top_p: Some(0_f32),
         n: Some(2),
--- src/lib.rs
+++ src/lib.rs
@@ -19,7 +19,7 @@
 //!     let openai = OpenAI::new(auth, "https://api.openai.com/v1/");
 //!     let body = ChatBody {
 //!         model: "gpt-3.5-turbo".to_string(),
-//!         max_tokens: Some(7),
+//!         max_completion_tokens: Some(7),
 //!         temperature: Some(0_f32),
 //!         top_p: Some(0_f32),
 //!         n: Some(2),
